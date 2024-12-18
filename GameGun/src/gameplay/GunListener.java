0d86782b37b5223c5cdc34126e3b3178632a0453
Ahmed-Khamis22 <ahmedelian2016@gmail.com>
2024-12-18 03:41:56 +0200

    added GunListener

---
+++ GameGun/src/gameplay/GunListener.java
@@ -0,0 +1,15 @@
+package gameplay;
+
+import java.awt.event.KeyListener;
+import javax.media.opengl.GLEventListener;
+
+/**
+ *
+ * @author Tyba
+ */
+
+public abstract class GunListener implements GLEventListener, KeyListener {
+
+    protected String assetsFolderName = "Assets//Images";
+
+}
