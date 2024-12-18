9a827a7ac30db6c0487379dbbe3ad7b526aa4061
khalbs22 <113267668+khalbs22@users.noreply.github.com>
2024-12-18 03:09:32 +0200

    Blood CLass

---
+++ GameGun/src/gameplay/BloodEffect.java
@@ -0,0 +1,11 @@
+package gameplay;
+
+public class BloodEffect {
+    double x, y;
+    int timer;
+    public BloodEffect(double x, double y, int timer) {
+        this.x = x;
+        this.y = y;
+        this.timer = timer;
+    }
+}
